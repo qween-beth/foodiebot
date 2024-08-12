2393d9f056f931f37e4c614f5c5fdcc9d6d15e25
qween-beth <103456558+qween-beth@users.noreply.github.com>
2024-08-12 12:22:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class_labels = {0: 'apple', 1: 'banana', 2: 'beans', 3: 'beef', 4: 'chicken', 5:
 
 # Streamlit UI
 st.set_page_config(page_title="Food Analyzer", layout="wide")
-st.title("🍽️ AI Food Analyzer")
+st.title("🍽️ ChowBot | AI Food Analyzer")
 
 # API Key input
 api_key = st.text_input("Enter your Gemini API Key", type="password")
