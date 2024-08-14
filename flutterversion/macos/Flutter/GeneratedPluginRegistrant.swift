a7d8a915130e55072bc5ff3d0594f8276d82300f
qween-beth <103456558+qween-beth@users.noreply.github.com>
2024-08-14 02:12:42 -0700

    flutter image plugin

--- flutterversion/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutterversion/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import package_info_plus
 import path_provider_foundation
 import serious_python_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SeriousPythonPlugin.register(with: registry.registrar(forPlugin: "SeriousPythonPlugin"))
--- flutterversion/windows/flutter/generated_plugin_registrant.cc
+++ flutterversion/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_selector_windows/file_selector_windows.h>
 #include <serious_python_windows/serious_python_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
   SeriousPythonWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SeriousPythonWindowsPluginCApi"));
 }
